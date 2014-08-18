48e713872521dde2916f47949fa1083977413325
Philip Durbin <philip_durbin@harvard.edu>
2014-08-18 09:49:45 -0400

    stubbed out Vagrant CentOS VM to try setup #1

---
+++ scripts/vagrant/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "Setting up milestone-reader"
+rpm -Uvh http://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.noarch.rpm
+yum install -y python-pip
+cd /milestone_reader
+# FIXME: pip-build-root/Pillow errors
+pip install -r requirements/local.txt
+cd /milestone_reader/milestone_reader/milestone_reader/settings
+cp github_api_secrets_template.json github_api_secrets.json 
+cd /milestone_reader/milestone_reader
+# FIXME: need non-interactive setup
+#python manage.py syncdb
+echo "Before running, be sure to edit github_api_secrets.json"
