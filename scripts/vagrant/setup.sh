f3d622f18e581c523e6db92c8e6aa284f80792c6
Philip Durbin <philip_durbin@harvard.edu>
2014-08-19 14:35:39 -0400

    use virtualenvwrapper, local settings, auto start #1

--- scripts/vagrant/setup.sh
+++ scripts/vagrant/setup.sh
@@ -2,13 +2,18 @@
 echo "Setting up milestone-reader"
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.noarch.rpm
 yum install -y python-pip python-devel
+pip install virtualenvwrapper
+source /usr/bin/virtualenvwrapper.sh
+export WORKON_HOME=~/.virtualenvs
+mkvirtualenv milestones
+workon milestones
 cd /milestone_reader
 pip install -r requirements/local.txt
 cd /milestone_reader/milestone_reader/milestone_reader/settings
 cp github_api_secrets_template.json github_api_secrets.json 
 cd /milestone_reader/milestone_reader
-# FIXME: need non-interactive setup
-# python manage.py syncdb
+python manage.py syncdb --settings=milestone_reader.settings.local
 # 0.0.0.0 for vagrant port forwarding to work 
-# python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 --settings=milestone_reader.settings.local &
+# FIXME: getting DoesNotExist at /milestone-reader-admin/
 echo "Before running, be sure to edit github_api_secrets.json"
