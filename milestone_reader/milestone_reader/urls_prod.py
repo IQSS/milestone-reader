3e3ab4417ccd977054d77ff52865762bca8d7549
Raman Prasad <raman_prasad@harvard.edu>
2014-08-18 23:28:24 -0400

    add prod urls

---
+++ milestone_reader/milestone_reader/urls_prod.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
+
+    # Examples:
+    # url(r'^$', 'milestone_reader.views.home', name='home'),
+    url(r'^milestones/', include('apps.milestones.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^milestone-reader-admin/', include(admin.site.urls)),
+)
+
