1673feb348c7d3c53e103b49eff9695cc3ba2164
Philip Durbin <philip_durbin@harvard.edu>
2014-08-19 21:49:35 -0400

    resolve "Site matching query does not exist" #1
    - also set up superuser account (root:root)

--- scripts/vagrant/setup.sh
+++ scripts/vagrant/setup.sh
@@ -13,7 +13,10 @@ cd /milestone_reader/milestone_reader/milestone_reader/settings
 cp github_api_secrets_template.json github_api_secrets.json 
 cd /milestone_reader/milestone_reader
 python manage.py syncdb --settings=milestone_reader.settings.local
+# resolve "Site matching query does not exist" and setup root:root
+# http://stackoverflow.com/questions/11476210/getting-site-matching-query-does-not-exist-error-after-creating-django-admin/23028198#23028198
+# http://stackoverflow.com/questions/1466827/automatically-create-an-admin-user-when-running-djangos-manage-py-syncdb
+python manage.py loaddata /milestone_reader/scripts/vagrant/fixtures.json --settings=milestone_reader.settings.local
 # 0.0.0.0 for vagrant port forwarding to work 
 python manage.py runserver 0.0.0.0:8000 --settings=milestone_reader.settings.local &
-# FIXME: getting DoesNotExist at /milestone-reader-admin/
 echo "Before running, be sure to edit github_api_secrets.json"
