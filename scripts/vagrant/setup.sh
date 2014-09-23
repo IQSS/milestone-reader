cde022cfe37507e512f60b5b605f9a67ac587664
Philip Durbin <philip_durbin@harvard.edu>
2014-09-23 16:44:00 -0400

    worked on running under apache/wsgi #1

--- scripts/vagrant/setup.sh
+++ scripts/vagrant/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Setting up milestone-reader"
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/6Server/x86_64/epel-release-6-8.noarch.rpm
-yum install -y python-pip python-devel
+yum install -y python-pip python-devel httpd mod_wsgi
 pip install virtualenvwrapper
 source /usr/bin/virtualenvwrapper.sh
 export WORKON_HOME=~/.virtualenvs
@@ -13,7 +13,7 @@ cd /milestone_reader/milestone_reader/milestone_reader/settings
 cp github_api_secrets_template.json github_api_secrets.json 
 cd /milestone_reader/milestone_reader
 python manage.py validate --settings=milestone_reader.settings.local
-python manage.py syncdb --settings=milestone_reader.settings.local
+python manage.py syncdb --noinput --settings=milestone_reader.settings.local
 # resolve "Site matching query does not exist" and setup root:root
 # http://stackoverflow.com/questions/11476210/getting-site-matching-query-does-not-exist-error-after-creating-django-admin/23028198#23028198
 # http://stackoverflow.com/questions/1466827/automatically-create-an-admin-user-when-running-djangos-manage-py-syncdb
@@ -23,9 +23,20 @@ python manage.py loaddata apps/repositories/fixtures/iqss_initial.json --setting
 # Note: `rm -rf /milestone_reader/test_setup` to start over
 # python manage.py dumpdata --indent=4 milestones
 cp /milestone_reader/milestone_reader/milestone_reader/settings/github_api_secrets.json.local /milestone_reader/milestone_reader/milestone_reader/settings/github_api_secrets.json
+exit
 python /milestone_reader/milestone_reader/apps/milestones/milestone_retriever.py
 # start django
 # python /milestone_reader/scripts/vagrant/runserver.sh
 # FIXME: why can't we start django programatically from here?
 # Should use Apache or nginx anyway, of course. :)
-echo "Next steps: vagrant ssh, sudo -i, /milestone_reader/scripts/vagrant/runserver.sh"
+#echo "Next steps: vagrant ssh, sudo -i, /milestone_reader/scripts/vagrant/runserver.sh"
+echo "Setting up Apache"
+mkdir /var/www/milestones
+mkdir /var/www/milestones/media # user uploads
+mkdir /var/www/milestones/static # images, js, css, etc.
+mkdir /var/www/milestones/milestones # wsgi.py
+python manage.py collectstatic --noinput
+cp /milestone_reader/milestone_reader/milestone_reader/vagrant-centos-wsgi.py /var/www/milestones/milestones/wsgi.py
+cp /milestone_reader/deploy/vagrant-centos-milestones.conf /etc/httpd/conf.d/milestones.conf
+service httpd start
+chkconfig httpd on
